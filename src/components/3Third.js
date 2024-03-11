a98247de2c560aa73d10a6d21e15d5afa9a6862e
Misterlinux <mrlzarate8@gmail.com>
2024-03-11 17:02:34 +0100

    third panel design

--- src/components/3Third.js
+++ src/components/3Third.js
@@ -51,6 +51,13 @@ function Terzo(){
   }), [terzoView])
 
   //THE COL-12 will cover the 100% of the current aviable space, 
+  //rememeber that the space is 78vw in total for teh side column
+
+  //For the position absolute we need the Absolute image to be before its relative one
+  //if not the 2 will overlay
+  //Position absolute elements TAKEN OUYT OF document flow, they dont modify
+  //so if its forst on teh container it will overlay with teh next (that wont change due to it)
+  //and if placed below it will start from the end of the previous, without overlaying
   return(   
     <div style={{marginLeft: "22%", height: "100vh"}} ref={terzo}>
 
@@ -58,26 +65,36 @@ function Terzo(){
         <h1>This is the duo</h1>
       </div>
 
-      <div className="d-flex justify-content-center row mx-0 col-12">
+      <div className="d-flex justify-content-center align-items-center row mx-0 px-2 col-12 mt-5">
 
-        <div className="col-12 row mx-0 px-1 d-flex align-items-center" style={{height: "40vh"}}>
-
-          <div className="col-4 px-0" style={{height: "100%"}}>
-            <div className="d-flex justify-content-center align-items-center position-relative" style={{overflow: "hidden", borderLeft: "2px solid red"}}>
-              <div className="d-flex justify-content-center align-items-center" style={{width: "100%", height: "40vh", backgroundColor: "purple" }}>
-                <FontAwesomeIcon icon={faMusic} style={{fontSize: "5em"}}/>
-              </div>
-
-              <animated.div className="position-absolute centered" 
-                style={{width: "100%", height: "40vh", left: ruolo[0].x ,backgroundImage: `url(${basso})` }}>
+        <div className="row col-12 mx-0 px-0 d-flex justify-content-start align-items-start position-relative" style={{height: "40vh"}}>
+          <div className="position-relative d-flex px-0" 
+            style={{top: "2%", width: "100%", height: "30vw", zIndex: 5 }}>
+            <div style={{height: "100%", width: "30vw" }}>
+              
+              <animated.div className="thirdImage position-absolute" 
+                style={{backgroundImage: `url(${basso})`, height: "100%", width: "30vw", left: ruolo[0].x}}>
               </animated.div>
-            </div>  
+
+              <div className="d-flex justify-content-center align-items-center" 
+                style={{width: "30vw", height: "100%", backgroundColor: "purple"}}>
+                <FontAwesomeIcon icon={faMusic} style={{fontSize: "3em"}}/>
+              </div>
+            </div>
+
+            <div className="bg-success text-white text-start d-flex flex-column justify-content-center align-items-center" 
+              style={{height: "100%", width: "45vw"}}>
+              <h3 className="ps-1">Watt Watterson</h3>
+              <p>The main member
+                <FontAwesomeIcon icon={faMusic} />
+              </p>
+            </div>
           </div>
-          <div className="col-8">
-            <h3>Watt Watterson</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. 
-              <FontAwesomeIcon icon={faMusic} />
-            </p>
+
+          <div className="col-10 bg-success position-absolute px-2" 
+            style={{height: "90%", top: "0%", right: "0%", paddingTop: "calc(30vw + 4%)"}}>
+ 
+            <p> Bandinst of teh group, expert in drumns and ready to show his new drum </p>
 
             <animated.div style={{width: 50, height: 50, x: walk[0].x}}>
               <animated.div style={{height: "100%", width: "100%",
@@ -97,23 +114,46 @@ function Terzo(){
                 </animated.div>
               </div>
             </div>
-          </div>
 
+          </div>
         </div>
 
 
-        <div className="col-12 row mx-0 mt-3" style={{height: "30vh"}}>
-          <div className="col-8">
-            <h3>Watt Watterson</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. 
-              <FontAwesomeIcon icon={faMusic} />
-            </p>
+        <div className="row col-12 mx-0 px-0 d-flex justify-content-end align-items-start position-relative" style={{height: "40vh"}}>
+          <div className="position-relative d-flex px-0"
+            style={{top: "2%", width: "100%", height: "30vw", zIndex: 5 }}>
 
-            <animated.div style={{width: 50, height: 50, x: walk[1].x}}>
+            <div className="bg-success text-white d-flex flex-column justify-content-center align-items-center" 
+              style={{height: "100%", width: "45vw"}}>
+              <h3 className="ps-1">Watt Watterson</h3>
+              <p>The main member
+                <FontAwesomeIcon icon={faMusic} />
+              </p>
+            </div>
+
+            <div style={{height: "100%", width: "30vw" }}>
+              <animated.div className="thirdImage position-absolute" 
+                style={{backgroundImage: `url(${basso})`, height: "100%", width: "30vw", right: ruolo[0].x}}>
+              </animated.div>
+
+              <div className="d-flex justify-content-center align-items-center" 
+                style={{width: "30vw", height: "100%", backgroundColor: "purple"}}>
+                <FontAwesomeIcon icon={faMusic} style={{fontSize: "3em"}}/>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="col-10 bg-success position-absolute px-2" 
+            style={{height: "90%", top: "0%", left: "0%", paddingTop: "calc(30vw + 2%)"}}>
+
+            <p> Bandinst of teh group, expert in drumns and ready to show his new drum </p>
+
+            <animated.div style={{width: 50, height: 50, x: walk[0].x}}>
               <animated.div style={{height: "100%", width: "100%",
-                  backgroundImage: `url(${ice})`,
-                  backgroundPosition,
-                }}
+                backgroundImage: `url(${ice})`,
+                backgroundPosition,
+              }}
               >
               </animated.div>
             </animated.div>
@@ -123,22 +163,11 @@ function Terzo(){
               </div>
 
               <div className="d-flex align-items-center position-absolute">
-                <animated.div style={{height: 3, width: pros[1].width, backgroundColor: "red" }}>
+                <animated.div style={{height: 3, width: pros[0].width, backgroundColor: "red", transformOrigin: "right" }}>
                 </animated.div>
               </div>
             </div>
 
-          </div>
-          <div className="col-4">
-            <div className="d-flex justify-content-center align-items-center position-relative" style={{overflow: "hidden", borderRight: "2px solid red"}}>
-              <div className="d-flex justify-content-center align-items-center" style={{width: "100%", height: "40vh", backgroundColor: "purple" }}>
-                <FontAwesomeIcon icon={faMusic} style={{fontSize: "5em"}}/>
-              </div>
-
-              <animated.div className="position-absolute centered" 
-                style={{width: "100%", height: "40vh", left: ruolo[1].x  ,backgroundImage: `url(${basso})` }}>
-              </animated.div>
-            </div>
           </div>
         </div>
 
